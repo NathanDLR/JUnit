3d9d5a2fe08c96781c1a8b1683ff5e6e509d757c
NathanDLR <natandelreal@gmail.com>
2021-02-16 11:08:02 +0100

    Initial app

---
+++ JUnitTest/src/junittest/JUnitTest.java
@@ -0,0 +1,16 @@
+package junittest;
+
+/**
+ *
+ * @author natan
+ */
+public class JUnitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
